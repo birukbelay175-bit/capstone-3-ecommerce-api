989ac537d267ffbbac6e4d926bdf83b6dcdfa26a
birukbelaytafese <birukbelay175@gmail.com>
2026-06-23 14:24:33 -0500

    OrderService commit

--- src/main/java/org/yearup/service/OrderService.java
+++ src/main/java/org/yearup/service/OrderService.java
@@ -1,4 +1,67 @@
 package org.yearup.service;
 
-public class OrderService {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yearup.models.*;
+import org.yearup.repository.OrderLineItemRepository;
+import org.yearup.repository.OrderRepository;
+
+import java.time.LocalDateTime;
+
+@Service
+public class OrderService
+{
+    private final OrderRepository orderRepository;
+    private final OrderLineItemRepository orderLineItemRepository;
+    private final ShoppingCartService shoppingCartService;
+    private final ProfileService profileService;
+
+    public OrderService(OrderRepository orderRepository,
+                        OrderLineItemRepository orderLineItemRepository,
+                        ShoppingCartService shoppingCartService,
+                        ProfileService profileService)
+    {
+        this.orderRepository = orderRepository;
+        this.orderLineItemRepository = orderLineItemRepository;
+        this.shoppingCartService = shoppingCartService;
+        this.profileService = profileService;
+    }
+
+    @Transactional
+    public Order checkout(int userId)
+    {
+        ShoppingCart cart = shoppingCartService.getByUserId(userId);
+        Profile profile = profileService.getByUserId(userId);
+
+        Order order = new Order();
+        order.setUserId(userId);
+        order.setDate(LocalDateTime.now());
+
+        if (profile != null)
+        {
+            order.setAddress(profile.getAddress());
+            order.setCity(profile.getCity());
+            order.setState(profile.getState());
+            order.setZip(profile.getZip());
+        }
+
+        order.setShippingAmount(0);
+
+        Order savedOrder = orderRepository.save(order);
+
+        for (ShoppingCartItem cartItem : cart.getItems().values())
+        {
+            OrderLineItem lineItem = new OrderLineItem();
+            lineItem.setOrderId(savedOrder.getOrderId());
+            lineItem.setProductId(cartItem.getProductId());
+            lineItem.setSalesPrice(cartItem.getProduct().getPrice());
+            lineItem.setQuantity(cartItem.getQuantity());
+
+            orderLineItemRepository.save(lineItem);
+        }
+
+        shoppingCartService.clearCart(userId);
+
+        return savedOrder;
+    }
 }
